e4fc1d7a4943ec106c16ea0184d0090bd783464e
manoj sharma <manoj.sharma.webdev@gmail.com>
2025-05-31 02:38:11 +0000

    option updated

--- src/constant/SideBarOption.js
+++ src/constant/SideBarOption.js
@@ -6,10 +6,13 @@ export const SidebarOptions = [
     { option_name: "India's Union Teretory", option_code: "in_ut" },
     { option_name: "India's Tropic Of Cancer", option_code: "in_tropic_of_cancer" },
     { option_name: "India's IST Line", option_code: "in_ist" },
-{ option_name: "India's Border", option_code: "in_border" },
-{ option_name: "Expansion of India", option_code: "in_expansion" },
-{ option_name: "India's Tiger Reserve", option_code: "in_tiger_reserve" },
-{ option_name: "India's National Park", option_code: "in_national_park" },
-{ option_name: "India's BioSphere Reserve", option_code: "in_bsr" },
+    { option_name: "India's Border", option_code: "in_border" },
+    { option_name: "Expansion of India", option_code: "in_expansion" },
+    { option_name: "India's Tiger Reserve", option_code: "in_tiger_reserve" },
+    { option_name: "Ganga River Systen", option_code: "in_ganga_river" },
+    { option_name: "Sindhu River System", option_code: "in_sindhu_river" },
+    { option_name: "Bhramaputra River System", option_code: "in_brahmaputra_river" },
+    // { option_name: "India's National Park", option_code: "in_national_park" },
+    // { option_name: "India's BioSphere Reserve", option_code: "in_bsr" },
 
 ]
